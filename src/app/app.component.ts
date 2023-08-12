06646478fc11affd071e21be4bd9a1e6c1d30b86
JoaoSilva1711 <joaomm.silva@hotmail.com>
2023-08-13 00:44:45 +0100

    landing page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StarWarsLib';
+
+  playHoverSound() {
+    const audio = document.getElementById('hoverSound') as HTMLAudioElement;
+    audio.play();
+  }
+
+  
 }
